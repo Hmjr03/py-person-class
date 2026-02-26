900b4c08cc3f53022ba836910ceed6e2c025d5f1
Humberto Manhani Junior <hmanhani@hotmail.com>
2026-02-26 22:08:30 +0100

    Implement Person and create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    Person.people.clear()
+
+    person_list = []
+
+    for data in people:
+        person = Person(data["name"], data["age"])
+        person_list.append(person)
+
+    for data in people:
+        person = Person.people[data["name"]]
+
+        if "wife" in data and data["wife"] is not None:
+            person.wife = Person.people[data["wife"]]
+
+        if "husband" in data and data["husband"] is not None:
+            person.husband = Person.people[data["husband"]]
+
+    return person_list
