3692d3bd46a93d445fa5947bc088f8d1c7bbb9e6
Humberto Manhani Junior <hmanhani@hotmail.com>
2026-02-26 23:04:47 +0100

    New Solution

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
 class Person:
-    people = {}
+    people: dict[str, "Person"] = {}
 
     def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
+        self.name: str = name
+        self.age: int = age
+        self.wife: "Person | None" = None
+        self.husband: "Person | None" = None
         Person.people[name] = self
 
 
 def create_person_list(people: list[dict]) -> list["Person"]:
     Person.people.clear()
 
-    person_list = []
-
-    for data in people:
-        person = Person(data["name"], data["age"])
-        person_list.append(person)
+    person_list = [Person(data["name"], data["age"]) for data in people]
 
     for data in people:
         person = Person.people[data["name"]]
 
-        if "wife" in data and data["wife"] is not None:
-            person.wife = Person.people[data["wife"]]
+        wife_name = data.get("wife")
+        if wife_name:
+            person.wife = Person.people[wife_name]
 
-        if "husband" in data and data["husband"] is not None:
-            person.husband = Person.people[data["husband"]]
+        husband_name = data.get("husband")
+        if husband_name:
+            person.husband = Person.people[husband_name]
 
     return person_list
